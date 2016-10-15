2e8ea1135ff3f202ebf90a8e5ea8ddddde55f90a
mitrevf <mitrevf@gmail.com>
2016-10-15 15:09:02 +0200

    Sudoku Solver

---
+++ sudoku/sudoku/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(15, 25);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(1179, 964);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Naložena slika";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1232, 550);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 49);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reši sudoku";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1350, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Slika po transformacija";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(1353, 496);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(425, 493);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(1353, 25);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(450, 450);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1200, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 65);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Nalozi slika";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1350, 480);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Zaznane stevilke";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(1200, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(147, 82);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Metoda za razpoznavanje";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(11, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(117, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Podobnost vzorcev";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(11, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(130, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "vgrajena OCR metoda";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1232, 496);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 48);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Poglej polja";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1908, 992);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ sudoku/sudoku/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.OCR;
+using Emgu.CV.UI;
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+
+namespace sudoku
+{
+    public partial class Form1 : Form
+    {
+        int c = 0;
+        List<Image<Gray, Byte>> fields=new List<Image<Gray, byte>>();
+        Tesseract tess;
+        int[,] grid = new int[9, 9];
+        List<Image<Gray, byte>> vzorci = new List<Image<Gray, byte>>();
+        bool SolveSudoku(int[,] grid)
+        {
+            int row = 0;int col = 0;
+            if (!FindUnassignedLocation(grid,ref row,ref col))
+                return true; // success!
+            for (int num = 1; num <= 9; num++)
+            {
+                if (isSafe(grid, row, col, num))
+                {
+                    grid[row,col] = num;
+                    if (SolveSudoku(grid))
+                        return true;
+                    grid[row,col] = 0;
+                }
+            }
+            return false;
+        } 
+        bool FindUnassignedLocation(int[,] grid, ref int row,ref int col)
+        {
+            for (row = 0; row < 9; row++)
+                for (col = 0; col < 9; col++)
+                    if (grid[row,col] == 0)
+                        return true;
+            return false;
+        }
+        bool UsedInRow(int[,] grid, int row, int num)
+        {
+            for (int col = 0; col < 9; col++)
+                if (grid[row,col] == num)
+                    return true;
+            return false;
+        }
+
+        bool UsedInCol(int[,] grid, int col, int num)
+        {
+            for (int row = 0; row < 9; row++)
+                if (grid[row,col] == num)
+                    return true;
+            return false;
+        }
+         bool UsedInBox(int[,] grid, int boxStartRow, int boxStartCol, int num)
+        {
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    if (grid[row + boxStartRow,col + boxStartCol] == num)
+                        return true;
+            return false;
+        }
+
+        bool isSafe(int[,] grid, int row, int col, int num)
+        {
+            return !UsedInRow(grid, row, num) &&
+                   !UsedInCol(grid, col, num) &&
+                   !UsedInBox(grid, row - row % 3, col - col % 3, num);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 10; i++)
+                vzorci.Add(new Image<Gray, byte>(i.ToString() + "vzorec.png"));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (SolveSudoku(grid) == false)
+            {
+                MessageBox.Show("neresljivo");
+                return;
+            }
+            NarisiStevilke();
+        }
+
+        private void button2_Click(object sender, EventArgs e)  //procesiranje slike
+        {
+            c = 0;fields = new List<Image<Gray, byte>>(); grid = new int[9, 9];
+            OpenFileDialog opf = new OpenFileDialog();
+            if (opf.ShowDialog() != DialogResult.OK)
+                return;
+            Image<Gray, Byte> gray = new Image<Gray, byte>(opf.FileName);
+            imageBox1.Image = gray.Clone();
+            Image<Gray, Byte> izhod = new Image<Gray, byte>(gray.Width, gray.Height);
+            // binariziranje(adaptivna pragovna segmentacija)
+            izhod = gray.ThresholdAdaptive(new Gray(255), Emgu.CV.CvEnum.AdaptiveThresholdType.MeanC, Emgu.CV.CvEnum.ThresholdType.BinaryInv, 11, new Gray(11));
+            izhod._SmoothGaussian(1); //glajenje, (ni obvezno potrebno)
+           // imageBox2.Image = izhod.Clone();
+            var nova = izhod.Clone();
+            //posopek za iskanje najvecjega povezanega objekta
+            VectorOfVectorOfPoint vvp = new VectorOfVectorOfPoint();
+            Mat hierarchy = new Mat();
+            CvInvoke.FindContours(izhod, vvp, hierarchy, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);
+            int largest_contour_index = 0;
+            double largest_area = 0;
+            VectorOfPoint largestContour;
+            for (int i = 0; i < vvp.Size; i++)
+            {
+                double a = CvInvoke.ContourArea(vvp[i], false);  
+                if (a > largest_area)
+                {
+                    largest_area = a;
+                    largest_contour_index = i;               
+                }
+            }
+            largestContour = vvp[largest_contour_index];
+            Point[] lc = largestContour.ToArray();
+            //iskanje kote za perspektivna transformacija
+            Point topleft = new Point(gray.Width, gray.Height);
+            Point topright = new Point(0, gray.Height);
+            Point botright = new Point(0, 0);
+            Point botleft = new Point(gray.Width, 0);
+            foreach (Point p in lc)
+            {
+                if ((p.X + p.Y) < (topleft.X + topleft.Y))
+                    topleft = p;
+                else if ((p.X - p.Y) > (topright.X - topright.Y))
+                    topright = p;
+                else if ((p.X + p.Y) > (botright.X + botright.Y))
+                    botright = p;
+                else if ((p.Y - p.X) > (botleft.Y - botleft.X))
+                    botleft = p;
+            }
+            //prerisemo gridlines, da se znebimo linije, potem ostanejo le stevilke(lazja razpoznava)
+            CvInvoke.DrawContours(nova, vvp, largest_contour_index, new MCvScalar(0, 0, 0),6, Emgu.CV.CvEnum.LineType.EightConnected, hierarchy, 1);
+            Image<Gray, Byte> warp = new Image<Gray, byte>(450, 450);
+            PointF[] src = new PointF[] { topleft, topright, botright, botleft };
+            PointF[] dst = new PointF[] { new Point(0, 0), new Point(450, 0), new Point(450, 450), new Point(0, 450) };
+            Mat warpmat = CvInvoke.GetPerspectiveTransform(src, dst); //racunamo matrika za transformacija
+            CvInvoke.WarpPerspective(nova, warp, warpmat, new Size(450, 450)); //izvedemo transformacija
+            //imageBox1.Image = nova;
+            imageBox2.Image = warp;
+            //warp._Erode(1); //krcenje ali sirjenje, ni potrebno
+            //warp._Dilate(1);
+
+            //razpoznava stevilk, 2 moznosti (izbira so radiogumbov)
+            if (radioButton1.Checked)
+                tess = new Tesseract(@"C:/Emgu/emgucv-windows-universal 3.0.0.2157/bin/", null, OcrEngineMode.Default, "123456789 ");
+
+            fields = new List<Image<Gray, byte>>(); //hranim polja za lazjo debagiranje
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    Image<Gray, Byte> temp = (warp.GetSubRect(new Rectangle(j * 50+3, i * 50+3, 44, 44))).Clone(); //malo izpustimo po robu polja
+                    temp._SmoothGaussian(1);
+                    Gray sum = temp.GetSum(); //ce ni dovolj beli pikslov(dele objektov), ni stevilka.
+                    if (sum.Intensity < 30000)
+                        continue;
+
+                    //spet iscemo najvecji element v polju, predvidevam da je stevilo
+                    VectorOfVectorOfPoint vvptemp = new VectorOfVectorOfPoint();
+                    Mat hierarchytemp = new Mat();
+                    CvInvoke.FindContours(temp, vvptemp, hierarchytemp, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);
+                    int ind = 0;
+                    double area = 100;
+                    VectorOfPoint contour;
+                    for (int k = 0; k < vvptemp.Size; k++)
+                    {
+                        double ar = CvInvoke.ContourArea(vvptemp[k], false);  
+                        if (ar > area)
+                        {
+                            area = ar;
+                            ind = k;                
+                        }
+                    }
+                    if (area == 100)
+                        continue; //ce je najvecjega kontura area manj kot 100 (polje je 44x44), povzamemem da ni stevilo
+                    contour = vvptemp[ind]; //kontura stevlike
+                    
+                    var tempimg = new Image<Gray, Byte>(44, 44, new Gray(0));
+                    CvInvoke.DrawContours(tempimg, vvptemp, ind, new MCvScalar(255, 0, 0), -1, Emgu.CV.CvEnum.LineType.EightConnected, hierarchytemp);
+                    //narisemo notranjosti najvecjega kontura v novi sliki z belo barvo
+                    fields.Add(tempimg); //dodamo za pogled
+                    if (radioButton2.Checked)
+                    {
+                        Rectangle br = CvInvoke.BoundingRectangle(contour);
+                        int indeks = 0;
+                        double vrednost = double.MaxValue;
+                        for (int q = 0; q < 9; q++)
+                        {   //racunamo podobnost s vsakem vzorcu
+                            var kraj = tempimg.GetSubRect(new Rectangle(br.X, br.Y, vzorci[q].Width, vzorci[q].Height));
+                            var pod = vzorci[q].AbsDiff(kraj);
+                            var podobnost = pod.GetSum();
+                            if (podobnost.Intensity < vrednost)
+                            {
+                                indeks = q + 1; //ker je zero based
+                                vrednost = podobnost.Intensity;
+                            }
+                        }
+                        grid[i, j] = indeks;//najbolj podobni je zaznana stevilka
+                    }
+                    else
+                    {
+                        tess.Recognize(tempimg); //raspoznava slike s pomocjo tesseract OCR vgrajen v openCV
+                        var x = tess.GetCharacters();
+                        if (x.Length == 1)
+                            grid[i, j] = Convert.ToInt32(x[0].Text);
+                    }
+                }
+            NarisiStevilke(); //izpisemo stevilki v polje
+        }
+        void NarisiStevilke() //metoda za risanje matrika s stevili v textbox
+        {
+            string tmp = "";
+            for (int i=0;i<9;i++)
+            {
+                for (int j=0;j<9;j++)
+                {
+                    if (grid[i, j] != 0)
+                        tmp += grid[i, j].ToString();
+                    else
+                        tmp += " ";
+                    if (j != 8)
+                        tmp += "\t";
+                }
+                if (i!=8)
+                    tmp += "\n";
+            }
+            richTextBox1.Text = tmp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (fields != null && fields.Count > 0)
+            {
+                imageBox1.Image = fields[c];
+                c++;
+                if (c == fields.Count)
+                    c = 0;
+            }
+        }
+    }
+}
